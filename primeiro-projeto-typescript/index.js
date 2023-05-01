c26629e6366372da198a55c75e61b2ae9e658edd
Joe <joedsonfera@hotmail.com>
2023-05-01 19:57:45 -0300

    typescript

---
+++ primeiro-projeto-typescript/index.js
@@ -0,0 +1,6 @@
+var saudacao = function (nome) {
+    return "ola ".concat(nome);
+};
+saudacao("Joe Mendes");
+var nome = "Joao";
+var idade = 32;
---
+++ primeiro-projeto-typescript/index.ts
@@ -0,0 +1,8 @@
+const saudacao = (nome: string): string => {
+    return `ola ${nome}`;
+}
+
+console.log(saudacao("Joe Mendes"));
+
+const nome: string = "Joao";
+const idade: number = 32;
